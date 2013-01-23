a6e2c9fe1a8984823617e4695d6c0ba143aee063
cabadol <cbadenes@gmail.com>
2013-01-23 13:54:47 +0100

    first commit!

--- src/main/java/asciidoc/maven/plugin/AsciiDocMojo.java
+++ src/main/java/asciidoc/maven/plugin/AsciiDocMojo.java
@@ -6,18 +6,9 @@
  * http://www.gnu.org/licenses/gpl.html
  * 
  * Contributors:
+ *     cbitar - initial API and implementation
  *     Carlos Badenes Olmedo - initial API and implementation
  ******************************************************************************/
-/*
- * Copyright (c) 2011-2012 Charbel Bitar.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl-2.0.html
- * 
- * Contributors:
- *     cbitar - initial API and implementation
- */
 package asciidoc.maven.plugin;
 
 import java.io.File;
